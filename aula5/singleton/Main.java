d61bd42831f72c02e042f8676668fc68b095eda0
Dodas_ <ph005009004@gmail.com>
2024-09-22 22:05:36 -0300

    arquivos do projeto de revisão

--- aula5/singleton/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        FabricaDeCarro fabricaDeCarro = FabricaDeCarro.getInstance();
-
-        System.out.println(fabricaDeCarro.description);
-    }
-}
--- banksystem/src/main/java/me/dodas/models/Client.java
+++
@@ -1,8 +0,0 @@
-package me.dodas.models;
-
-public class Client {
-    public String name;
-    public String cpf;
-    public String email;
-    public String telefone;
-}
